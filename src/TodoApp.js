80f7d305ad737d831ec35c252175690c2f68125d
Younes Zaid <zaidyounes94@gmail.com>
2018-07-17 17:04:20 +0100

    Adding toggled todo and Editing ui

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -3,28 +3,42 @@ import React, { Component } from 'react';
 
 let nextTodo = 0;
 class TodoApp extends Component {
-
+  state = {
+    todoTitle: ''
+  }
   render() {
     const { todos, store } = this.props;
+    const { todoTitle } = this.state;
     return(
       <div className="todo-container">
-        <form>
-          <input/>
+        <div>
+          <input type="texte" value={todoTitle} onChange={(e) => {
+            this.setState({
+              todoTitle: e.target.value
+            })
+          }}/>
           <button onClick={(e) => {
             // alert("clicked");
             e.preventDefault();
             store.dispatch({
               type: 'ADD_TODO',
-              text: 'TEST',
+              text: todoTitle,
               id: nextTodo++
             })
+            this.setState({ todoTitle: '' })
           }}>
             Add todo
           </button>
-        </form>
+        </div>
         <ul>
           {todos.map(todo => 
-            <li key={todo.id}>
+            <li key={todo.id} onClick={() => {
+              store.dispatch({
+                type: "TOGGLE_TODO",
+                id: todo.id
+              })
+            }} style={{ textDecoration: todo.completed ? 'line-through' : 'none'}}>
+              {/* {todo.completed ? <i class="zmdi zmdi-badge-check"></i> : ''} */}
               {todo.text}
             </li>
           )}
